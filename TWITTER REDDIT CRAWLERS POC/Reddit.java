2d42a5935fd786c04156f50daf4b50f3cb34352e
jellyvongelatin <how2pocketsg@gmail.com>
2020-02-06 22:55:09 +0800

    Update Reddit.java

--- TWITTER REDDIT CRAWLERS POC/Reddit.java
+++ TWITTER REDDIT CRAWLERS POC/Reddit.java
@@ -13,23 +13,27 @@ public class Reddit {
 
     public void crawlReddit() throws IOException {
         System.out.println("==========CRAWLING REDDIT SEARCH==========");
-        Elements searchResults = doc.select("div.search-result-link header.search-result-header > a[href], div.search-result-link div.search-result-meta > span"); //looks for links under the search results for links' header.
+        Elements searchResults = doc.select("div.search-result-link header.search-result-header > a[href], div.search-result-link div.search-result-meta > span,  span.search-time > time[title]").not("span.search-result-icon"); //looks for links under the search results for links' header.
         if(searchResults.isEmpty()){
             System.out.println("FUCK");
         }
         int i = 1; //index of posts
         String url = "";
         for (Element result : searchResults) {
-            //every 4 elements: title, comments, author, subreddit
+            //title, (blank?), points, exact post timestamp, OP, subreddit
             if (result.tagName().equals("a")){
-                if (i != 1) {
+                if (i != 1 || i == searchResults.size()) {
                     crawlComments(url);
                 }
-                System.out.println("text: " + result.text());
+                System.out.println("\n==================================POST=======================================================");
+                System.out.println("Post Title: " + result.text());
                 url = result.attr("abs:href");
             }
+            else if (result.tagName().equals("time")){
+                System.out.println("Posted on: " + result.attr("title"));
+            }
             else{
-                System.out.println("text: " + result.text());
+                System.out.println(result.text());
             }
             i++;
         }
@@ -38,11 +42,23 @@ public class Reddit {
     public void crawlComments(String url) throws IOException {
         System.out.println("==========CRAWLING COMMENTS==========");
         Document commentsPage = Jsoup.connect(url).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
-        Elements comments = commentsPage.select("p.tagline a.author, div.thing div.usertext-body div.md > p");
+        Elements comments = commentsPage.select("div.commentarea p.tagline > a.author, p.tagline > span.author, p.tagline > span.unvoted, div.commentarea p.tagline > time[title], div.thing div.usertext-body div.md > p");
         for (Element comment : comments){
             //every 2 elements: commenter username, comment
-            if (comment.tagName().equals("a"))
+            if (comment.tagName().equals("a")) {
                 System.out.println("\nComment by: " + comment.text());
+            }
+            else if (comment.tagName().equals("time")){
+                System.out.println("Posted: " + comment.attr("title"));
+            }
+            else if (comment.tagName().equals("span")){
+                if (comment.attr("class").equals("score unvoted")){
+                    System.out.println("Score: "+ comment.text());
+                }
+                else{
+                    System.out.println("\nDeleted Comment: " +comment.text());
+                }
+            }
             else{
                 System.out.println(comment.text());
             }
