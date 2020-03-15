9e97d7b3b020d3b73adda7b6a5aa53f515655cf0
jellyvongelatin <how2pocketsg@gmail.com>
2020-03-15 17:24:11 +0800

    cpp

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
-#include "Crawler.h"
+#include "Crawler/Crawler.h"
+#include "Post Objects/RedditPost.h"
+// #include "Post Objects/RedditComment.h"
 
 using namespace std;
 
 int main(void)
 {
-    Crawler crawler;
+
     string input;
 
     // Start program
     cout << "ICT1009 C++ Crawler by Team __" << endl;
 
-    // Constatntly prompts user
+    // Constatntly prompts user until exit
     while (1 == 1)
     {
         // Take user input
-        cout << "Please select what you would like to do:" << endl;
-        cout << "> Crawl\n> Load (Must have existing saved crawled data!)\n> Statistics\n> Export\n> Exit" << endl;
-        cout << "Input: ";
+        cout << "\nPlease select what you would like to do:" << endl;
+        cout << " > Crawl\n > Load\n > Statistics\n > Export\n > Help\n > Exit" << endl;
+        cout << "\nInput: ";
         cin >> input;
 
         // Convert to lower case
@@ -28,40 +30,69 @@ int main(void)
         //Executes functions
         if (input.compare("exit") == 0)
         {
-            cout << "Goodbye!" << endl;
+            cout << "\nGoodbye!" << endl;
             break;
         }
         else if (input.compare("crawl") == 0)
         {
-            // Crawls for data
+
+            // Collects defined search query
             string query;
-            cout << "Please enter a search query: ";
+            cout << "\nPlease enter a search query: ";
             cin >> query;
 
+            //Crawls for data
+            Crawler crawler;
 
+            cout << "Data crawled for '" << query << "'!\nYou may now export using the 'Export' command or display statistics with the 'Statistics' command." << endl;
         }
         else if (input.compare("load") == 0)
         {
-            // Loads data from existing file
+            // Prompts user for filename
             string filename;
-            cout << "Please enter filename: ";
+            cout << "\nPlease enter filename: ";
             cin >> filename;
+
+            // Loads data from existing file
+            // if no file is found, print error
+            // if file is found, load into objects
         }
         else if (input.compare("statistics") == 0)
         {
             // Displays statistics for crawled data
+            // if no data found in objects, print error
+            // if data is found, display stats
             cout << "Displaying Statistics for ___" << endl;
         }
         else if (input.compare("export") == 0)
         {
-            // Exports data to file
+            // Prompts for filename
             string filename;
-            cout << "Please enter filename: ";
+            cout << "\nPlease enter filename: ";
             cin >> filename;
+
+            // Exports data to file
+            // if no data present, print error
+            // if data is present, export to file
+        }
+        else if (input.compare("help") == 0)
+        {
+            cout << "\n> Crawl" << endl;
+            cout << "\tCrawls for data from Reddit and __ based on entered search query.\n"
+                 << endl;
+            cout << "> Load" << endl;
+            cout << "\tLoads crawled data from existing file.\tMust have previously exported data.\n"
+                 << endl;
+            cout << "> Statistics" << endl;
+            cout << "\tDisplays statistics from the crawled data.\n\tMust have either crawled or imported data.\n"
+                 << endl;
+            cout << "> Export" << endl;
+            cout << "\tExports crawled data to a file.\n\tMust have existing crawled or imported data.\n"
+                 << endl;
         }
         else
         {
-            cout << "Please enter a valid input!" << endl;
+            cout << "\nPlease enter a valid input!" << endl;
         }
     }
 
